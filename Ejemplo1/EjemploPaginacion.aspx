403fe695c4850f359b10db99b76449e4131066ff
ReimerGhost <rchamale10@gmail.com>
2015-05-02 18:57:01 -0600

    Ejemplo de uso de parametros de URL e inserción de HTML desde C#.

---
+++ Ejemplo1/EjemploPaginacion.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EjemploPaginacion.aspx.cs" Inherits="IPC2Ejemplos.Ejemplo1.EjemploPaginacion" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+        <asp:Label ID="htmlCode" runat="server" Text="HELLO"></asp:Label>
+    <a href=EjemploPaginacion.aspx></a>
+    </form>
+</body>
+</html>
---
+++ Ejemplo1/EjemploPaginacion.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IPC2Ejemplos.Ejemplo1
+{
+    public partial class EjemploPaginacion : System.Web.UI.Page
+    {
+        String[] nombres;
+        int[] valores;
+        int p;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try{
+                p = int.Parse(Request.QueryString["pag"]);
+            }catch{
+                p = 1;
+            }
+                
+            asignarDatos();
+            htmlCode.Text = entablarHTMLContenido(escribirCatalogo(nombres, valores, p, 6))
+                + "" + imprimeHTMLPaginas(6) ;
+        }
+
+        //Asumiendo que éste metodo obtiene los resultados de SQL Server
+        private void asignarDatos()
+        {
+            //Obtener resultados por medio de WebService (que retorne un array[])
+            String[] n = { "Dato1", "Dato2", "Dato3", "Dato4", "Dato5", "Dato6",
+                           "Dato7", "Dato8", "Dato9", "Dato10", "Dato11", "Dato12",
+                           "Dato13", "Dato14", "Dato15", "Dato16", "Dato17", "Dato18", "Dato19", "Dato20",
+                         "Dato1", "Dato2", "Dato3", "Dato4", "Dato5", "Dato6",
+                           "Dato7", "Dato8", "Dato9", "Dato10", "Dato11", "Dato12",
+                           "Dato13", "Dato14", "Dato15", "Dato16", "Dato17", "Dato18", "Dato19", "Dato20" };
+            //Obtener resultados por medio de WebService (que retorne un array[])
+            int[] v = { 100, 200, 300, 400, 500, 600,
+                        700, 800, 900, 1000, 1100, 1200,
+                        1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000,
+                        100, 200, 300, 400, 500, 600,
+                        700, 800, 900, 1000, 1100, 1200,
+                        1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000 };
+            //Asignando resultados a variables locales.
+            nombres = n;
+            valores = v;
+        }
+
+        private String imprimeHTMLPaginas(int m)
+        {
+            String paginas="";
+            int maxP = nombres.Length/m;
+            for (int i = 1;i<=maxP;i++)
+            {
+                if(i==p){
+                    paginas +=i+" ";
+                }else{
+                paginas += " <a href=\"EjemploPaginacion.aspx?pag="+i+"\">" + i + "</a>";
+                }
+            }
+            return paginas;
+        }
+
+        private String escribirCatalogo(String[] prod, int[] precio, int pag, int items)
+        {
+            String fila="";
+
+            if (pag > 0)
+            {
+                int inicia;
+                if(pag == 1)
+                {
+                    inicia = 0;
+                }
+                else
+                {
+                    inicia = (items*(pag-1));
+                }
+                if ((inicia + items) < prod.Length)
+                {
+                    items = inicia + items;
+                }
+                else
+                {
+                    items = prod.Length;
+                }
+                for (int i = inicia; i < items; i++)
+                {
+                    fila += agregarFila(agregarColumna(prod[i] + "</br>" + precio[i]));
+                }
+            }
+            else
+            {
+                fila = "ERROR";
+            }
+
+            return fila;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cont"></param>
+        /// <returns></returns>
+        private String agregarColumna(String cont)
+        {
+            return "<td>" + cont + "</td>";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cont"></param>
+        /// <returns></returns>
+        private String agregarFila(String cont)
+        {
+            return "<tr>" + cont + "</tr>";
+        }
+
+
+        private String entablarHTMLContenido(String cont)
+        {
+            String tabla;
+            tabla = "<table border=\"1px\">";
+            tabla += cont;
+            tabla += "</table>";
+            return tabla;
+        }
+    }
+}
